13d0ab1638cf8741ad9ff387eb5388c8127cc8f9
Senzen <sentheseventh@gmail.com>
2022-12-18 15:45:28 +0100

    mend

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 if DEBUG == False:
-    STATIC_ROOT = [BASE_DIR / 'static',]
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
