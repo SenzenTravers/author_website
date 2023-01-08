d467b5bc5292e1fb18acc4689f4f03656771412b
Senzen <sentheseventh@gmail.com>
2023-01-08 12:17:29 +0100

    Attempt number 79873168.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,6 +5,4 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 from django.core.wsgi import get_wsgi_application
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    application = get_wsgi_application()
+application = get_wsgi_application()
