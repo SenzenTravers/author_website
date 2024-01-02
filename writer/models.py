d270acff2625b4331f6abbaafdfe459ddb459d5c
Senzen <sentheseventh@gmail.com>
2024-01-02 21:54:34 +0100

    fix: improve post lengths

--- writer/models.py
+++ writer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     date = models.DateField()
-    body = models.TextField(max_length=3000)
+    body = models.TextField(max_length=6000)
 
     def __str__(self):
         return f"{self.date} : {self.body[:100]}..."
