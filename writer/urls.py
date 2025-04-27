ae721e37e9b2cd236665e73edf0a256c923d5bec
Senzen <sentheseventh@gmail.com>
2025-04-27 13:21:00 +0200

    feat: opened up Voiture Noire part of the site

--- writer/urls.py
+++ writer/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.Homepage.as_view(), name='homepage'),
     path('about', views.about, name='about'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
