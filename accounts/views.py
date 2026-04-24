93c58925a84f124bebeacabee06013b9f0f270e2
Senzen <sentheseventh@gmail.com>
2026-04-24 22:39:23 +0200

    feat: birthday feature

--- accounts/views.py
+++ accounts/views.py
@@ -7,13 +7,9 @@ from django.views import generic, View
 from accounts.models import Member
 
 
-# Create your views here.
 class Birthday(View):
     def get(self, request, *args, **kwargs):
-        # TODO: get either date or user_id
-        # Todo: later on, change for class BirthdayHandler(generic.View):
-        # Get date from request, default to today if not provided
-        date_param = request.GET.get('date') or request.POST.get('date')
+        date_param = request.GET.get('date')
 
         if date_param:
             try:
@@ -23,16 +19,41 @@ class Birthday(View):
         else:
             query_date = date.today()
 
-        profiles = Member.objects.filter(birthday__isnull=False)
+        profiles = Member.objects.filter(birthday__isnull=False).exclude(discord_id="")
         data = [
-            {
-                'member_id': member.id if member else None,
-                'member_username': member.username if member else None,
-                'birthday': member.birthday.isoformat() if member.birthday else None
-            }
-            for member in profiles if member.birthday and member.birthday == query_date
+            {'discord_ids': member.discord_id} for member in profiles
+            if member.birthday.day == query_date.day and member.birthday.month == query_date.month
         ]
         return JsonResponse({'members_birthdays': data, 'query_date': query_date.isoformat()})
 
     def post(self, request, *args, **kwargs):
-        pass
+        date_param = request.POST.get('birthday')
+        discord_id_param = request.POST.get('discord_id')
+
+        if discord_id_param == '' or date_param == '':
+            return JsonResponse(
+            {'errorMsg': "Expecting a 'birthday' field (YYYY-MM-DD) and a 'discord_id' field."},
+            status=400
+        )
+
+        try:
+            query_date = datetime.strptime(date_param, '%Y-%m-%d').date()
+        except:
+            return JsonResponse(
+            {'errorMsg': "Expecting a 'birthday' field (YYYY-MM-DD) and a 'discord_id' field."},
+            status=400
+        )
+
+        try:
+            member = Member.objects.get(discord_id=discord_id_param)
+        except:
+            return JsonResponse(
+            {'errorMsg': "No member with that discord id."},
+            status=404
+        )
+
+        birthday_person = Member.objects.get(discord_id=discord_id_param)
+        birthday_person.birthday = datetime.strptime(date_param, '%Y-%m-%d').date()
+        birthday_person.save()
+
+        return JsonResponse({'errorMsg':""})
--- tests/accounts/test_birthdays.py
+++ tests/accounts/test_birthdays.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from django.test import TestCase
 from django.urls import reverse
 from accounts.models import Member
@@ -6,42 +6,120 @@ from accounts.models import Member
 
 class AccountsBirthdaysTestCase(TestCase):
     def setUp(self):
-        # Create 3 members
-        # - One without birthdate
-        self.member1 = Member.objects.create_user("user1", email="user1@test.com", password="pass", birthday=None)
-        # - One with the requested birthdate (2024-12-25)
-        self.member2 = Member.objects.create_user(
-            "user2", email="user2@test.com", password="pass", birthday=date(2024, 12, 25)
+        self.today = date.today()
+        self.date_plus_ten = date.today() + timedelta(days=10)
+        self.date_minus_ten = date.today() - timedelta(days=10)
+
+        self.no_birthday = Member.objects.create_user(
+            "no_birthday", email="no_birthday@test.com", password="pass", birthday=None,
+            discord_id='17871183718936171'
         )
-        # - One with a different birthdate
-        self.member3 = Member.objects.create_user(
-            "user3", email="user3@test.com", password="pass", birthday=date(2024, 6, 15)
+        self.has_requested_birthday = Member.objects.create_user(
+            "has_requested_birthday", email="has_requested_birthday@test.com",
+            password="pass", birthday=self.date_plus_ten,
+            discord_id='17871183718936172'
+        )
+        self.has_requested_birthday_but_no_discord_ids = Member.objects.create_user(
+            "has_requested_birthday_no_discord_id",
+            email="has_requested_birthday_no_discord_id@test.com",
+            password="pass", birthday=self.date_plus_ten
         )
 
-    def test_discord_profiles_birthdays_with_a_requesting_date(self):
-        # Login as one of the members
-        self.client.login(username="user1", password="pass")
-        # Request with a specific date
-        response = self.client.get(reverse('members:birthdays'), {'date': '2024-12-25'})
-        
-        self.assertEqual(response.status_code, 200)
+        self.has_different_birthday = Member.objects.create_user(
+            "has_different_birthday", email="has_different_birthday@test.com",
+            password="pass", birthday=self.date_minus_ten,
+            discord_id='17871183718936173'
+        )
+        self.has_today_birthday = Member.objects.create_user(
+            "has_today_birthday", email="has_today_birthday@test.com",
+            password="pass", birthday=date.today(),
+            discord_id='17871183718936174'
+        )
+
+    ############################### GET
+    def test_get_returns_users_matching_requesting_date_and_having_discord_ids(self):
+        requested_date = self.date_plus_ten.isoformat()
+
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.get(reverse('members:birthdays'), {'date': requested_date})        
         data = response.json()
         
-        # Should only return the profile with the matching birthdate
+        # Should only return profile(s) with a matching birthday
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data['members_birthdays']), 1)
-        self.assertEqual(data['members_birthdays'][0]['member_username'], 'user2')
-        self.assertEqual(data['members_birthdays'][0]['birthday'], '2024-12-25')
-        self.assertEqual(data['query_date'], '2024-12-25')
+        self.assertEqual(data['query_date'], requested_date)
 
-    def test_discord_profiles_birthdays_without_a_requesting_date(self):
-        # Login as one of the members
-        self.client.login(username="user1", password="pass")
-        # Request without a specific date
-        response = self.client.get(reverse('members:birthdays'), { })
-        
-        self.assertEqual(response.status_code, 200)
+    def test_get_returns_empty_if_no_users_matches_requested_date(self):
+        requested_date = date.today() - timedelta(days=20)
+        requested_date = requested_date.isoformat()
+
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.get(reverse('members:birthdays'), {'date': requested_date})
         data = response.json()
         
-        # Should only return the profile with the matching birthdate
+        # Should only return profile(s) with a matching birthday
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data['members_birthdays']), 0) 
+        self.assertEqual(data['query_date'], requested_date)
+
+    def test_get_without_args_returns_members_born_today_date(self):
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.get(reverse('members:birthdays'), { })
+        data = response.json()
+        
+        # Will return the member born on today's date
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data['members_birthdays']), 1) 
         self.assertEqual(data['query_date'], date.today().isoformat())
+
+    ################################## POST
+    def test_post_with_proper_birthday(self):
+        birthday = date.today() - timedelta(days=20)
+        birthday_iso = birthday.isoformat()
+
+
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.post(
+            reverse('members:birthdays'),
+            {'discord_id': '17871183718936171','birthday': birthday_iso}
+        )
+        members = Member.objects.filter(birthday__isnull=False).filter(discord_id__isnull=False)
+        data = [
+            {'discord_ids': member.discord_id} for member in members
+            if member.birthday.day == birthday.day and member.birthday.month == birthday.month
+        ]
+        # Will return the member with a changed birthday
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 1) 
+        self.assertEqual(data[0]['discord_ids'], '17871183718936171')
+
+    def test_post_without_birthday(self):
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.post(
+            reverse('members:birthdays'),
+            {'discord_id': '17871183718936171'}
+        )
+        data = response.json()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            data['errorMsg'],
+            "Expecting a 'birthday' field (YYYY-MM-DD) and a 'discord_id' field."
+        )
+
+    def test_post_with_birthday_no_matching_discord_id(self):
+        birthday = date.today() - timedelta(days=20)
+        birthday = birthday.isoformat()
+
+        self.client.login(username="no_birthday", password="pass")
+        response = self.client.post(
+            reverse('members:birthdays'),
+            {'discord_id': 'WRONG_DISCORD_ID','birthday': birthday}
+        )
+        data = response.json()
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            data['errorMsg'],
+            "No member with that discord id."
+        )
